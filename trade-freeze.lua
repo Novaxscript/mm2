b87b89a63d658523cc98db3574ac7462277a00b4
Novaxscript <novaxscript06@gmail.com>
2026-09-11 19:25:58 +0300

    Update trade-freeze.lua

--- trade-freeze.lua
+++ trade-freeze.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/vprqbtwWfFmB9dS1/raw",true))()
+loadstring(game:HttpGet("https://arasaka-corp.eu/scripts/dQWbm2iQZRJeLW8T3PRvULjohSTD1Vd2ejn4GZ9wfP1Pkhgm.lua"))()
